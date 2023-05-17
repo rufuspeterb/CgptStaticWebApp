991d907137cd89a2c19dbba226544cd1de27c6d0
rufuspeterb <45489228+rufuspeterb@users.noreply.github.com>
2023-05-17 22:22:33 +0530

    v4
    v4

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Update the profile information
+  document.getElementById('name').innerText = 'John Doe';
+  document.getElementById('bio').innerText = 'I am a web developer with a passion for creating engaging user experiences.';
+
+  // You can add more logic here to fetch data from an API or database
+});
